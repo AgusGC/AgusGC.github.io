15379267726fbb653ae7cb69a326968a7792aa4d
Agustín Guerrero <agusguerrero345@gmail.com>
2024-07-26 15:31:44 -0300

    Responsive

---
+++ script.js
@@ -0,0 +1,9 @@
+window.addEventListener('load', () =>{
+    let menu_btn = document.getElementById('menu-btn');
+    let menu = document.getElementById('menu');
+
+    menu_btn.addEventListener('click', () =>{
+        menu.classList.toggle('mostrar');
+    })
+      
+})
